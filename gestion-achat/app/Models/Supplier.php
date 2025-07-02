8743b03875e6168478ac866ab468c93e51ff870d
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2025-07-02 10:27:59 +0300

    Migration and modele of Supplier

--- gestion-achat/app/Models/Supplier.php
+++ gestion-achat/app/Models/Supplier.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+
+    use HasFactory;
+
+    protected $table = 'suppliers';
+
+    protected $primaryKey = 'supplier_id';
+
+    protected $fillable = [
+        'name',
+        'contact_email',
+        'contact_phone',
+        'address',
+        'api_endpoint',
+        'api_key'
+    ];
+    
 }
--- gestion-achat/database/migrations/2025_07_02_061418_create_suppliers_table.php
+++ gestion-achat/database/migrations/2025_07_02_061418_create_suppliers_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id');
+            $table->string('name');
+            $table->string('contact_email');
+            $table->string('contact_phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('api_endpoint')->nullable();
+            $table->string('api_key')->nullable();
             $table->timestamps();
         });
     }
