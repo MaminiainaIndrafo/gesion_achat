2d1469cac434cc9a46e6f2eb9522b8aa8708b435
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2025-07-02 10:31:45 +0300

    Migration and model for product

--- gestion-achat/app/Models/Products.php
+++ gestion-achat/app/Models/Products.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $table = 'products';
+
+    protected $primaryKey = 'product_id';
+
+    protected $fillable = [
+        'supplier_id',
+        'reference',
+        'name',
+        'description',
+        'purchase_price',
+        'selling_price',
+        'shopify_price',
+        'shopify_id'
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'supplier_id');
+    }
 }
--- gestion-achat/database/migrations/2025_07_02_061419_create_products_table.php
+++ gestion-achat/database/migrations/2025_07_02_061419_create_products_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('product_id');
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
+            $table->string('reference')->unique(); // Référence fournisseur
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('purchase_price', 11, 5); // Prix d'achat HT
+            $table->decimal('selling_price', 11, 5)->nullable(); // Prix de vente conseillé
+            $table->decimal('shopify_price', 11, 5)->nullable(); // Prix Shopify
+            $table->string('shopify_id')->nullable(); // ID du produit dans Shopify
         });
     }
 
